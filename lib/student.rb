49a2aea9fb80783f379762c719f485c607722f24
Mac Dowell Prosper <macdowell@noukod.com>
2018-11-06 22:03:26 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,13 +1,13 @@
 class Student
   attr_accessor :name, :grade
-  attr_reader :id 
+  attr_reader :id
 
   def initialize name, grade, id=nil
-    @name, @grade, @id = name, grade, id 
+    @name, @grade, @id = name, grade, id
   end
 
   def self.create_table
-    sql = <--SQL CREAT TABLE students (
+    sql = <<-SQL CREAT TABLE students (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     grade TEXT
@@ -16,5 +16,5 @@ class Student
 
     DB[:conn].execute(sql)
   end
-  
+
 end
