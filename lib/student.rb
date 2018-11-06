495aa087d116bc2a1c1d152ec095154136ab25ff
Mac Dowell Prosper <macdowell@noukod.com>
2018-11-06 22:03:42 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,7 +7,8 @@ class Student
   end
 
   def self.create_table
-    sql = <<-SQL CREAT TABLE students (
+    sql = <<-SQL 
+    CREATE TABLE students (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     grade TEXT
