a4afc4f7f8f171a9ccac25f22961ded8d95e8cd5
Mac Dowell Prosper <macdowell@noukod.com>
2018-11-06 22:19:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,11 +27,13 @@ class Student
   end
 
   def save
-    sql = <<-SQL 
+    sql = <<-SQL
     INSERT INTO students (name,grade) VALUES (?,?)
     SQL
 
     DB[:conn].execute(sql,name, grade)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
 end
